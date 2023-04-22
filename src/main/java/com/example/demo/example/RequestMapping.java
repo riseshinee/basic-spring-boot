23d0ae8f7628fc121d1d1a93a9f54cc8d259bd83
seungheee <lsh3431@gmail.com>
2023-04-22 23:42:59 +0900

    ✨ request mapping test

---
+++ src/main/java/com/example/demo/example/RequestMapping.java
@@ -0,0 +1,38 @@
+package com.example.demo.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RequestMapping {
+
+    @GetMapping("/login/hello.do") // http://localhost/ch2/login/hello.do
+    public void test1(){
+        System.out.println("urlpattern=/login/hello.do");
+    }
+
+    @GetMapping("/login/*")   // /login/hello, /login/hi
+    public void test2(){
+        System.out.println("urlpattern=/login/*");
+    }
+
+    @GetMapping("/login/**/tmp/*.do")   // /login/tmp/hello.do, /login/aaa/tmp/hello.do
+    public void test3(){
+        System.out.println("urlpattern=/login/**/tmp/*.do");
+    }
+
+    @GetMapping("/login/??")
+    public void test4(){   // /login/hi, /login/my.car
+        System.out.println("urlpattern=/login/??");
+    }
+
+    @GetMapping("*.do") // /hello.do, /hi.do, /login/hi.do
+    public void test5(){ 
+        System.out.println("urlpattern=*.do");
+    }
+    
+    @GetMapping("/*.???") //  /hello.aaa, /abc.txt
+    public void test6(){ 
+        System.out.println("urlpattern=*.???");
+    }
+}
