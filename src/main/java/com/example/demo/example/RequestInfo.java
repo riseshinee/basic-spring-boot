5b3518265f5f6f6c6c2a355ff077a1866911b988
seungheee <lsh3431@gmail.com>
2023-04-23 23:04:02 +0900

    ✨ method example

--- src/main/java/com/example/demo/example/RequestInfo.java
+++ src/main/java/com/example/demo/example/RequestInfo.java
@@ -1,5 +1,10 @@
 package com.example.demo.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class RequestInfo {
     @RequestMapping("/requestInfo")
