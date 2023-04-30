a8fe9d40e9574814bab137c4c7d490ab078f8b32
seungheee <lsh3431@gmail.com>
2023-04-30 23:50:09 +0900

    ✨ mvc example for method

---
+++ src/main/java/com/example/demo/mvc/MethodInfo.java
@@ -0,0 +1,34 @@
+package com.example.demo.mvc;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.StringJoiner;
+
+public class MethodInfo {
+	public static void main(String[] args) throws Exception{
+        //객체 생성
+		Class clazz = Class.forName("com.example.demo.mvc.YoilTeller");
+		Object obj = clazz.newInstance();
+		
+        //모든 메서드의 정보를 가져와서 객체에 저장
+		Method[] methodArr = clazz.getDeclaredMethods();
+		
+		for(Method m : methodArr) {
+			String name = m.getName(); //메서드 이름
+			Parameter[] paramArr = m.getParameters(); //매개변수 목록
+//			Class[] paramTypeArr = m.getParameterTypes();
+			Class returnType = m.getReturnType(); //반환 타입
+			
+			StringJoiner paramList = new StringJoiner(", ", "(", ")");
+			
+			for(Parameter param : paramArr) {
+				String paramName = param.getName();
+				Class  paramType = param.getType();
+				
+				paramList.add(paramType.getName() + " " + paramName);
+			}
+			
+			System.out.printf("%s %s%s%n", returnType.getName(), name, paramList);
+		}
+	} // main
+}
